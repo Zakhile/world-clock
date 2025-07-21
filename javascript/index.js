776ec46d8ff7c82e76559c2a5813b41ed752559d
Zakhile <zakhilezulu@gmail.com>
2025-07-21 10:45:35 +0200

    Update index.js
    Added a static script , so there is a foundation for when the dynamic script is added

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
-console.log("hello, world!");
+// JavaScript code to update the world clock for Centurion
+let centurionElement = document.getElementById("centurion-clock");
+let centurionDateElement = centurionElement.querySelector(".date");
+let centurionTimeElement = centurionElement.querySelector(".time");
+centurionTimeElement.innerHTML = "07:10:05 <small>AM</small>"; // Example static time
+centurionDateElement.textContent = "21 Jul 2025"; // Example static date
+
+// JavaScript code to update the world clock for Johannesburg
+let johannesburgElement = document.getElementById("johannesburg-clock");
+let johannesburgDateElement = johannesburgElement.querySelector(".date");
+let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+johannesburgTimeElement.innerHTML = "07:10:05 <small>AM</small>"; // Example static time
+johannesburgDateElement.textContent = "21 Jul 2025"; // Example static date
